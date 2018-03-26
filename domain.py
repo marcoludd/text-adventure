51f4dc640f5490cac86ddc6bf56f24a9e28ab577
marcoludd <marcoludd@gmail.com>
2018-03-26 10:29:57 -0300

    whitespaces

--- domain.py
+++ domain.py
@@ -6,18 +6,21 @@ import player
 
 
 class Game:
-	def __init__(self):
-		self.view = view.View()
-	def start_game(self):
-		while True:
-            choice = self.view.start()
-            if choice == 2
-                self.view.terminate()
-                break			
-            if choice == 1
-    #if opcao == criar personag
-    player.sdiufsaf hdiu
+    def __init__(self):
+        self.view = view.View()
 
-1if __name__ == 'main':
-	main = Game()
-	main.start_game()
+    def start_game(self):
+
+        while True:
+            choice = self.view.start()
+            if choice == 2:
+                self.view.terminate()
+            break
+            if choice == 1:
+                # if opcao == criar personag
+                #player.create()
+
+
+if __name__ == 'main':
+    main = Game()
+    main.start_game()

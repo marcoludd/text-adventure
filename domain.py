48175700ed4782ee9ea28922cb94ae45b5195eef
marcoludd <marcoludd@gmail.com>
2018-03-26 13:16:56 -0300

    menus

--- domain.py
+++ domain.py
@@ -10,15 +10,13 @@ class Game:
 
     def start_game(self):
         while True:
-            choice = self.view.start()
+            choice = self.view.start_menu()
             if choice == 0:
                 self.view.terminate()
                 break
             if choice == 1:
                 player_one = player.Player(self.view.ask_name(),self.view.ask_job())
-                print (player_one.name)
-                print (player_one.job)
-
+                
 if __name__ == '__main__':
     main = Game()
     main.start_game()
--- view.py
+++ view.py
@@ -3,13 +3,25 @@
 
 
 class View:
-    def start(self):
+    def start_menu(self):
         print('Welcome to the very basic text-adventure game\n')
         print('1: Create Character \n'
-              '2: Nothing \n'
               '0: Exit Game \n')
         return int(input('\nChoice: '))
+    
+    def player_menu(self):
+        print('Your journey has started')
+        print('e Equipment \n'
+              's Stats \n'
+              'l Change Level\n')
+        return input('')
 
+    def level_menu(self):
+        print('Description')
+        print('Monster')
+        print('Treasure')
+        print('Leave')
+        
     def terminate(self):
         print('\nFarewell adventurer')
 

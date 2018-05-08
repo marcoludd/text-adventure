71bb071f2acba9c4f454059a420ab6b8b7213769
Marco Ludwig <marcoludd@gmail.com>
2018-05-08 19:17:13 -0300

    hp

--- persona.py
+++ persona.py
@@ -8,4 +8,11 @@ class Persona:
         self.strength = strength
         self.speed = speed
         self.magic = magic
+        self.hp = (2*(self.strength+self.speed))*(1 + self.magic)
+    
+    def hp_mod(self):
+        self.hp = (2*(self.strength+self.speed))*(1 + self.magic)
+
+    def str_mod(self, number):
+        self.hp += number
 
